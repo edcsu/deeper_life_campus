dd53eaf736dd826a54c3e254b7e9eec0a9b9a8f4
Keith Ssewanonda <keith@laboremus.no>
2020-11-21 17:01:48 +0300

    chore:add constants

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF064681);
+const kPrimaryLightColor = Color.fromARGB(255,66,133,244);
+const kSecondaryColor = Color(0xFF858FD0);
+const kTextColor = Color(0xFF505050);
