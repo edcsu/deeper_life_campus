3c7960515720692b7887e72ae28726646608ed08
Keith Ssewanonda <keith@laboremus.no>
2020-11-28 21:04:48 +0300

    style:forms

---
+++ lib/screens/forms/addattendance/components/add_attendance_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddAttendanceBody extends StatefulWidget {
+  AddAttendanceBody({Key key}) : super(key: key);
+
+  @override
+  _AddAttendanceBodyState createState() => _AddAttendanceBodyState();
+}
+
+class _AddAttendanceBodyState extends State<AddAttendanceBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text("yolo"),
+    );
+  }
+}
---
+++ lib/screens/forms/addfinance/components/add_finance_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddFinanceBody extends StatefulWidget {
+  AddFinanceBody({Key key}) : super(key: key);
+
+  @override
+  _AddMemberBodyState createState() => _AddMemberBodyState();
+}
+
+class _AddMemberBodyState extends State<AddFinanceBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Text("yolo"),
+    );
+  }
+}
