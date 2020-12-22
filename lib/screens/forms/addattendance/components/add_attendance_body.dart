b393904d3d3a0c916f84a5c123eb2dc5a1d6b4b7
Keith Ssewanonda <keith@laboremus.no>
2020-12-22 18:33:38 +0300

    feat:add attendance form details

--- lib/screens/forms/addattendance/components/add_attendance_body.dart
+++ lib/screens/forms/addattendance/components/add_attendance_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reactive_forms/reactive_forms.dart';
 
 class AddAttendanceBody extends StatefulWidget {
   AddAttendanceBody({Key key}) : super(key: key);
@@ -8,10 +9,75 @@ class AddAttendanceBody extends StatefulWidget {
 }
 
 class _AddAttendanceBodyState extends State<AddAttendanceBody> {
+  final attendanceForm = FormGroup({
+    'typeOfService': FormControl<String>(validators: [Validators.required]),
+    'attendance': FormControl<int>(validators: [Validators.required]),
+    'dateOfService': FormControl<DateTime>(validators: [Validators.required])
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text("yolo"),
+    return ReactiveForm(
+      formGroup: this.attendanceForm,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: <Widget>[
+            ReactiveTextField(
+              formControlName: 'typeOfService',
+              decoration: InputDecoration(
+                labelText: 'Type Of Service',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ReactiveTextField(
+              formControlName: 'attendance',
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Attendance',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ReactiveTextField(
+              formControlName: 'dateOfService',
+              readOnly: true,
+              decoration: InputDecoration(
+                labelText: 'Date',
+                suffixIcon: ReactiveDatePicker(
+                  formControlName: 'dateOfService',
+                  firstDate: DateTime(1990),
+                  lastDate: DateTime(2100),
+                  builder: (context, picker, child) {
+                    return IconButton(
+                      onPressed: picker.showPicker,
+                      icon: Icon(Icons.date_range),
+                    );
+                  },
+                ),
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 40.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ReactiveFormConsumer(
+                  builder: (context, form, child) {
+                    return RaisedButton(
+                      child: Text('SUBMIT'),
+                      onPressed: form.valid ? () => print(form.value) : null,
+                    );
+                  },
+                ),
+                RaisedButton(
+                  child: Text('RESET'),
+                  onPressed: () => attendanceForm.reset(),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
