a4024a67f772077692961913bea72df9db1ac736
Keith Ssewanonda <keith@laboremus.no>
2020-12-22 17:25:15 +0300

    style:add member form inputs

--- lib/screens/forms/addmember/components/add_member_body.dart
+++ lib/screens/forms/addmember/components/add_member_body.dart
@@ -9,11 +9,126 @@ class AddMemberBody extends StatefulWidget {
 }
 
 class _AddMemberBodyState extends State<AddMemberBody> {
-  
   final form = FormGroup({
-    'name': FormControl(value: 'John Doe'),
-    'email': FormControl(value: 'johndoe@email.com'),
+    'firstName': FormControl<String>(validators: [Validators.required]),
+    'lastName': FormControl<String>(validators: [Validators.required]),
+    'email': FormControl<String>(
+        validators: [Validators.required, Validators.email]),
+    'phoneNumber': FormControl<String>(validators: [Validators.required]),
+    'dateOfBirth': FormControl<DateTime>(validators: [Validators.required]),
+    'department': FormControl<String>(validators: [Validators.required]),
+    'program': FormControl<String>(validators: [Validators.required]),
+    'country': FormControl<String>(validators: [Validators.required]),
+    'yearEnrolled': FormControl<DateTime>(validators: [Validators.required]),
+    'yearGraduated': FormControl<DateTime>(validators: [Validators.required]),
   });
+
+  List<Step> steps = [
+    Step(
+      title: const Text('Personal Information'),
+      isActive: true,
+      state: StepState.indexed,
+      content: Column(
+        children: <Widget>[
+          ReactiveTextField(
+            formControlName: 'firstName',
+            decoration: InputDecoration(
+              labelText: 'First Name',
+            ),
+          ),
+          ReactiveTextField(
+            formControlName: 'lastName',
+            decoration: InputDecoration(
+              labelText: 'Last Name',
+            ),
+          ),
+          ReactiveTextField(
+            formControlName: 'email',
+            decoration: InputDecoration(
+              labelText: 'Email',
+            ),
+          ),
+          ReactiveTextField(
+            formControlName: 'phoneNumber',
+            decoration: InputDecoration(
+              labelText: 'Phone number',
+            ),
+          ),
+          ReactiveTextField(
+            formControlName: 'dateOfBirth',
+            decoration: InputDecoration(
+              labelText: 'Date Of Birth',
+            ),
+            keyboardType: TextInputType.number,
+          ),
+        ],
+      ),
+    ),
+    Step(
+      isActive: false,
+      state: StepState.indexed,
+      title: const Text('Campus Information'),
+      content: Column(
+        children: <Widget>[
+          ReactiveTextField(
+            formControlName: 'department',
+            decoration: InputDecoration(
+              labelText: 'Department',
+            ),
+          ),
+          ReactiveTextField(
+            formControlName: 'program',
+            decoration: InputDecoration(
+              labelText: 'Program',
+            ),
+          ),
+          ReactiveTextField(
+            formControlName: 'country',
+            decoration: InputDecoration(
+              labelText: 'country',
+            ),
+          ),
+          ReactiveTextField(
+            formControlName: 'yearEnrolled',
+            decoration: InputDecoration(
+              labelText: 'Year Enrolled',
+            ),
+            keyboardType: TextInputType.number,
+          ),
+          ReactiveTextField(
+            formControlName: 'yearGraduated',
+            decoration: InputDecoration(
+              labelText: 'Year Graduated',
+            ),
+            keyboardType: TextInputType.number,
+          ),
+        ],
+      ),
+    ),
+  ];
+
+  int _currentStep = 0;
+  bool complete = false;
+
+  next() {
+    _currentStep + 1 != steps.length
+        ? goTo(_currentStep + 1)
+        : setState(() => complete = true);
+  }
+
+  cancel() {
+    if (_currentStep > 0) {
+      goTo(_currentStep - 1);
+    }
+  }
+
+  goTo(int step) {
+    setState(() => _currentStep = step);
+  }
+
+  void _onSubmit() {
+    print('The form is valid!');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +136,26 @@ class _AddMemberBodyState extends State<AddMemberBody> {
       formGroup: this.form,
       child: Column(
         children: <Widget>[
-          ReactiveTextField(
-            formControlName: 'name',
+          Flexible(
+            flex: 5,
+            child: Stepper(
+              steps: steps,
+              currentStep: _currentStep,
+              onStepContinue: next,
+              onStepTapped: (step) => goTo(step),
+              onStepCancel: cancel,
+            ),
           ),
-          ReactiveTextField(
-            formControlName: 'email',
-          ),
+          Flexible(
+            child: ReactiveFormConsumer(
+              builder: (context, form, child) {
+                return RaisedButton(
+                  child: Text('Submit'),
+                  onPressed: form.valid ? _onSubmit : null,
+                );
+              },
+            ),
+          )
         ],
       ),
     );
